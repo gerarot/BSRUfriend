95c3709f214864e45f87e9424084b354502fdc9e
gerarot <nithikan_artey@hotmail.co.th>
2017-02-09 15:15:32 +0700

    intent to SignUP

--- app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
+++ app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
@@ -1,13 +1,36 @@
 package mama.gerarot.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    // Explicit การประกาศตัวแปร
+    private Button signInButton, signUpButton;
+    private EditText userEditText, passEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget คือ การ Initial Var กับ View บน XML
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passEditText = (EditText) findViewById(R.id.editText3);
+
+        // Button Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+    } // Main Method
+}   // Maim Class นี่คือ คลาสหลัก
