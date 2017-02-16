ce6bd3d8f1d9c46284d126b05a2d8273fdebdc45
gerarot <nithikan_artey@hotmail.co.th>
2017-02-16 11:20:10 +0700

    Read JSON OK!!!!

--- app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
+++ app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
@@ -3,6 +3,7 @@ package mama.gerarot.bsrufriend;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     // Explicit การประกาศตัวแปร
     private Button signInButton, signUpButton;
     private EditText userEditText, passEditText;
+    private String userString, passString;
+    private String[] loginStrings;
+    private static final String urlPHP = "http://swiftcodingthai.com/bsru/get_user_artsist.php";
 
 
     @Override
@@ -32,5 +36,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Check Space and get Value Form Edit Text
+                userString = userEditText.getText().toString().trim();
+                passString = passEditText.getText().toString().trim();
+                if (userString.equals("") || passString.equals("")) {
+                    MyAlert myAlert = new MyAlert(MainActivity.this);
+                    myAlert.myDialog("คุณมีช่องว่างนะ","คุณต้องกรอกข้อมูลให้ครบนะ");
+                } else {
+                    //No Space
+                    checkUserPass();
+                }
+
+            }   //onClick
+        });
+
     } // Main Method
+
+    private void checkUserPass() {
+
+        try {
+
+            GetUser getUser = new GetUser(MainActivity.this);
+            getUser.execute(urlPHP);
+            String strJSON = getUser.get();
+            Log.d("16febV1", "strJSON ==>" + strJSON);
+
+
+        } catch (Exception e) {
+            Log.d("16febV1", "e checkUserPass ==>" + e.toString());
+        }
+
+    }   //checkUserPass
 }   // Maim Class นี่คือ คลาสหลัก
