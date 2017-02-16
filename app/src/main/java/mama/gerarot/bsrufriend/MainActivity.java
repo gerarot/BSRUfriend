7127fe55c67859017ccd7962bdd736f6a993ed67
gerarot <nithikan_artey@hotmail.co.th>
2017-02-16 11:55:12 +0700

    Check Password OK!!!!

--- app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
+++ app/src/main/java/mama/gerarot/bsrufriend/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -95,6 +96,14 @@ public class MainActivity extends AppCompatActivity {
                 //User False
                 MyAlert myAlert = new MyAlert(MainActivity.this);
                 myAlert.myDialog("หา user นี่ไม่เจอ", "ไม่มี" + userString + "ในฐานข้อมูลของเรา");
+            } else if (!passString.equals(loginStrings[3])) {
+                //Password False
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("Password False", "Please Try Again Password False");
+            } else {
+                //Password true
+                Toast.makeText(MainActivity.this, "Welcome "+loginStrings[1],
+                Toast.LENGTH_SHORT).show();
             }
 
         } catch (Exception e) {
